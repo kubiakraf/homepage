eb40ab15f9add7afe3bf2a4e55a86bfa05ee2317
Kubiak Rafał <rafal.kubiak@mail.com>
2023-07-21 13:47:57 +0200

    implementation of welcome on the home page. Removing it from the page about JS

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 
-alert('wtf!!');
-console.log("siema");
+const firstName = "Rafał";
+const age = 40;
 
-const name = "Rafał";
+alert('Witaj');
+
+alert(`Nazywam się ${firstName} i mam ${age} lat. Od pół roku uczę się pisać strony www, teraz próbuje zrobić to z kursem WTF - co ten frontend III edycja. Prowadzącym jest Macieja Korsan.`);
